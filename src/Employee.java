46adb2c5b207993af12f6d35aab6be8ec4ce8ec2
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-10-07 20:34:10 +0300

    second

--- src/Employee.java
+++ src/Employee.java
@@ -17,8 +17,35 @@ public class Employee {
         this.id = counter++;
     }
 
+    public String getName() {
+        return this.name;
+    }
+    public String getSurname() {
+        return this.surname;
+    }
+    public String getPatronymic() {
+        return this.patronymic;
+    }
+    public int getSection() {
+        return this.section;
+    }
+    public int getSalary() {
+        return this.salary;
+    }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setSection( int section) {
+        this.section = section;
+    }
+    public void setSalary( int salary) {
+        this.salary = salary;
+    }
+
+
     public String toString() {
-        return "Id: " + id + " FIO: " + name + surname + patronymic;
+        return "Id: " + id + "; FIO: " + name + surname + patronymic + "; Department: " + section + "; Salary: " + salary;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
+    public static void fullRosterPrint(Employee e) {
+                System.out.println(e);
+
+    }
     public static void main(String[] args) {
-        Employee Gil = new Employee("R ","G ","E ",1,2);
-        Employee Gil2 = new Employee("R ","G ","E ",1,2);
-        System.out.println(Gil);
-        System.out.println(Gil2);
+        Employee[] empl = new Employee[2];
+        empl [0] = new Employee("R ","G ","E ",1,20000);
+        empl [1] = new Employee("R1 ","G1 ","E1 ",1,30000);
+        fullRosterPrint(empl[1]);
+
 
     }
 }
