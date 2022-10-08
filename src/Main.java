b140d4e54ac168ca9b2e9a01657b2dfd28375e52
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-10-08 14:36:45 +0300

    third

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
-    public static void fullRosterPrint(Employee e) {
-                System.out.println(e);
-
+    public static void fullRosterPrint(Employee[] e) {
+        for (int i = 0; i < e.length; i++) {
+            System.out.println(e[i]);
+        }
     }
+
+    public static int salaryMonthSum(int s) {
+        int sum = 0;
+        sum = sum + s;
+        return sum;
+    }
+
     public static void main(String[] args) {
-        Employee[] empl = new Employee[2];
+        Employee[] empl = new Employee[2];  // Сделать [10]!
         empl [0] = new Employee("R ","G ","E ",1,20000);
         empl [1] = new Employee("R1 ","G1 ","E1 ",1,30000);
-        fullRosterPrint(empl[1]);
+
+        fullRosterPrint(empl);
+
+        for (int i = 0; i < empl.length; i++) {
+            salaryMonthSum(empl[i].getSalary());
+        }
+
+
 
 
     }
